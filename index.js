f17d7b94314c35938f5a68f0cfd61449908e3147
Iuri <iurisdesign@gmail.com>
2021-02-18 13:32:43 +0000

    creation of typing game

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-//TPC Typing game:
-//- Mostra-nos uma palavra aleatória
-// - 10 segundos para escrever essa palavra
-// - Se errarmos uma letra, começamos do inicio nessa palavra
-// - Se ficarmos sem tempo, perdemos
-// - Se acertamos, passamos à próxima palavra aleatória
+// Globals
+let wordToType = "";
+let indexCharacter = 0;
+let indexWordSelect = 0;
+let time = 10;
+let intervalID;
+let score = 0;
 
 // List of word in an array
 const wordsList = [
@@ -12,23 +13,77 @@ const wordsList = [
   "computer",
   "health",
   "imagination",
-  "Love",
+  "love",
 ];
+
+// DOM Elements
+const $wordToTypeDiv = document.querySelector("#wordToTypeDiv");
+const $time = $("#time");
 // Select a random word from the array for wordToType
-let wordToType;
-function selectRandomWordFrom(listOfWords) {
-  let randomIndex = Math.ceil(Math.random() * (wordsList.length - 1));
-  return listOfWords[randomIndex];
+
+function getRandomWord(listOfWords) {
+  indexWordSelect = Math.floor(Math.random() * wordsList.length);
+  return listOfWords[indexWordSelect];
 }
-wordToType = selectRandomWordFrom(wordsList);
-console.log(wordToType);
-wordToType = selectRandomWordFrom(wordsList);
-console.log(wordToType);
-// Show the wordToType in HTML
-const showWord2TypeTitle = document.querySelector("#wordToType");
-show;
-// Create events to each key of letters in words of the array
-// when the key of the first letter to paint is pressed, paint the letter and athorize to paint the next letter
-// If the player clicked other key than the letter to paint, reset the letterToPaint
-// When the wordToType is fully painted, show the next word to paint
-// If the time of 10 is over, Game Over
+
+function play() {
+  time = 10;
+  $time.text(time);
+  wordToType = getRandomWord(wordsList).split("");
+  displayWordToType();
+  indexCharacter = 0;
+  intervalID = setInterval(() => {
+    time -= 1;
+
+    if (time < 1) {
+      gameover();
+    }
+    $time.text(time);
+  }, 1000);
+}
+
+function displayWordToType() {
+  $wordToTypeDiv.innerHTML = "";
+  wordToType.forEach((char) => {
+    $wordToTypeDiv.innerHTML += `<span class="span-paint">${char}</span>`;
+  });
+}
+
+function checkWord() {
+  if (indexCharacter == wordsList[indexWordSelect].length) {
+    wordsList.splice(indexWordSelect, 1);
+    console.log(wordsList);
+    play();
+    clearInterval(intervalID);
+  }
+}
+
+function checkCharKey(key) {
+  const spanList = document.querySelectorAll(".span-paint");
+  if (key === wordToType[indexCharacter]) {
+    spanList[indexCharacter].classList.add("correct-color");
+    indexCharacter += 1;
+    checkWord();
+  } else {
+    displayWordToType();
+    indexCharacter = 0;
+  }
+}
+
+function gameover() {
+  console.log("gameover");
+  clearInterval(intervalID);
+  time = 0;
+}
+
+function starGame() {
+  $time.text(time);
+  play();
+}
+
+starGame();
+
+// Key event
+document.addEventListener("keyup", function (event) {
+  checkCharKey(event.key);
+});
